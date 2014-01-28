b4ea101571537cb5dae7e894f9e168ebad5fa5b4
aobi <aobi@aobi-Ubuntu>
2014-01-28 10:54:57 -0500

    Added new templates files and markups

---
+++ wp-content/themes/createnwjobs/single.php
@@ -0,0 +1,20 @@
+<?php
+get_header('single'); ?>
+ <div id="main-content1" class="row container show-border single-temp">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="col-md-8 addpad">
+            <h1 class="single-title"><?php the_title(); ?></h1>
+            <p class="byline"><?php the_date(); ?></p>
+            <p class="single-body"><?php the_content(); ?></p>
+            <?php endwhile; else: ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+        </div>
+        <div class="col-md-4">
+            <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+            <?php dynamic_sidebar( 'sidebar-1' ); ?>
+            <?php endif; ?> 
+        </div>
+        </div><!--End of main-content1 div-->
+
+<?php get_footer('single'); ?>
